77289ce42477afc8d4c334a42043190992599120
pankamol srikaew <aim.ske09@gmail.com>
2019-04-20 03:17:47 +0700

    Updates

---
+++ precache-manifest.3bdc7fe8065c544f92e3c6d4253937ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b23db03bb7f15e7b288",
+    "url": "/static/js/main.b91d27e0.chunk.js"
+  },
+  {
+    "revision": "5a62af67805b77e04e35",
+    "url": "/static/js/2.ce172de7.chunk.js"
+  },
+  {
+    "revision": "3b23db03bb7f15e7b288",
+    "url": "/static/css/main.d2607183.chunk.css"
+  },
+  {
+    "revision": "cb5e3b7b75586ec4140b5c6123779da2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58b0937e55d8627614436228a9ca7196.js"
+  "/precache-manifest.3bdc7fe8065c544f92e3c6d4253937ad.js"
 );
 
 workbox.clientsClaim();
