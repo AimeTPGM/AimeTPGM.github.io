4e22c5a17210a844280a89680075d133b7be10ca
pankamol srikaew <aim.ske09@gmail.com>
2019-04-27 22:13:35 +0700

    Updates

--- precache-manifest.70c4f8dff7cd73de5f4aaec29191b9ae.js
+++ precache-manifest.70c4f8dff7cd73de5f4aaec29191b9ae.js
@@ -24,19 +24,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "1500b885cc91e6e7e2e3",
-    "url": "/static/js/main.07e431ce.chunk.js"
+    "revision": "5498f1f3068952c45bdb",
+    "url": "/static/js/main.732f22d4.chunk.js"
   },
   {
     "revision": "1502d27789890612d2b4",
     "url": "/static/js/2.d13c4143.chunk.js"
   },
   {
-    "revision": "1500b885cc91e6e7e2e3",
+    "revision": "5498f1f3068952c45bdb",
     "url": "/static/css/main.aaf5ddb9.chunk.css"
   },
   {
-    "revision": "19070610e0c0b83e3dbf72868460e22b",
+    "revision": "173a0d695d6f49c955eb8410772e8754",
     "url": "/index.html"
   }
 ];
