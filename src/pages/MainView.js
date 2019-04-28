92924625b9242c93da3ab82fdbbf000dd8a352e9
pankamol srikaew <aim.ske09@gmail.com>
2019-04-29 03:56:09 +0700

    change header css

--- src/pages/MainView.js
+++ src/pages/MainView.js
@@ -6,7 +6,7 @@ import { mediaMaxWidth } from '../util/util';
 import MenuComponent from '../components/MenuComponent';
 import { lightBlue, masterHilightColor } from '../appConstant';
 
-const bracketStyle = { fontSize: '1.4em', fontWeight: '200', lineHeight: '0.7em' }
+const bracketStyle = { fontSize: '1.4em', fontWeight: '400', lineHeight: '0.7em' }
 export default () => (
     <div css={{
         alignItems: 'center',
@@ -17,7 +17,7 @@ export default () => (
             <a css={{ color: masterHilightColor, fontSize: '2em', textDecoration: 'none', display: 'flex', justifyContent: 'center'}} href={'#'}>
                             <div css={{ ...bracketStyle, marginRight: '10px' }}>{'<'}</div>
                             <div css={{marginRight: '10px'}}>
-                                <div css={{ textTransform: 'uppercase', fontWeight: '400' }}>Aime</div>
+                                <div css={{ textTransform: 'uppercase', fontWeight: '500' }}>Aime</div>
                                 <div css={{ fontSize: '0.3em', fontWeight: '100', lineHeight: '0.5em', letterSpacing: '7.5px'}}>P.Srikaew</div>
                             </div>
                             <div css={{ ...bracketStyle, margin: 0, fontSize: '1.35em', lineHeight: '1em' }}>{'/'}</div>
