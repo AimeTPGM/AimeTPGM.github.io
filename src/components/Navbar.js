b31ae82ea04e8792c51802fabf14fdd15ac209d8
AimeTPGM <aim.ske09@gmail.com>
2020-01-19 17:10:43 +0700

    restructure padding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,8 @@ export default () => {
     return (
    
         <div id="navbar" css={{ 
-            [mediaMaxWidth(768)]: { fontSize: '1em', padding: '20px' },
-            fontSize: '2em', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+            [mediaMaxWidth(768)]: { fontSize: '1em' },
+            fontSize: '2em', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '20px'}}>
             <Link to="/summary">
             <div css={{textTransform: 'uppercase'}}>
                 <div>Pankamol</div><div>Srikaew</div>
