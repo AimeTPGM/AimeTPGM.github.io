5a3bfabd2ba0b6c7c60138202ecf0a010bf22867
AimeTPGM <aim.ske09@gmail.com>
2020-07-28 02:11:25 +0700

    change typo

--- src/pages/AboutMeView.js
+++ src/pages/AboutMeView.js
@@ -19,8 +19,8 @@ export default () => {
             <div css={{ color: colorScheme.secondaryTextColor}}>Software Developer from Thailand</div>
             <div css={{fontFamily: 'Montserrat', marginTop: '70px', lineHeight: '1.6em'}}>
                 <div css={{ textIndent: '50px', lineHeight: '2em' }}>
-                    <div>I intensely passionate on development the full project life cycle from the start to delivery - and standing by 24/7 available every one week a month for project's production on-call support to make sure that our application is fully on servicing</div>
-                    <div>Besides contributing in project delivery, I would claim my cultural view of working to have a high priority on team's well-being and support all teammates' of their career growth. It's my pleasure to jump or hand in to the area that the my team needs supports to overcome the issue they are encountering. Also helping to improve our working process and facilitate my teammates to fully accomplish their in-hand tasks without worries.</div>
+                    <div>I am willing and intensely passionate on development the full project life cycle from the start to features delivery in challenging environment and standing by 24/7 available every one week a month for project's production on-call support to make sure that our application is fully on servicing.</div>
+                    <div>Besides contributing in project delivery, my cultural view of working has a focus on putting a high priority on team's well-being and support all team members' of their career growth. It's my pleasure to jump in if any of my team members need extra supports to solve the issue they are encountering. Also helping to improve our working process and facilitate my teammates to fully accomplish their in-hand tasks without worries.</div>
                     
                 </div>
                 <div css={{ marginTop: '30px'}}><span>If the qualities that has been mentioned above sounds like "someone you're looking for", let's find out how we could connect and contribute in the team.</span></div>
